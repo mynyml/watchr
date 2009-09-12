8212cf81a3515819f2e522afb93f1446b741a756
mynyml <mynyml@gmail.com>
2009-09-12 16:20:22 -0400

    Improve docs for Portable handler

--- lib/watchr/event_handlers/portable.rb
+++ lib/watchr/event_handlers/portable.rb
@@ -10,25 +10,35 @@ module Watchr
         @reference_mtime = Time.now
       end
 
+      # Enters listening loop.
+      #
+      # Will block control flow until application is explicitly stopped/killed.
+      #
       def listen(monitored_paths)
         @monitored_paths = monitored_paths
         loop { trigger; sleep(1) }
       end
 
-      def trigger
+      # See if an event occured, and if so notify observers.
+      def trigger #:nodoc:
         path, type = detect_event
         notify(path, type) unless path.nil?
       end
 
+      # Update list of monitored paths.
       def refresh(monitored_paths)
         @monitored_paths = monitored_paths
       end
 
       private
 
+      # Verify mtimes of monitored files.
+      #
+      # If the latest mtime is more recent than the reference mtime, return
+      # that file's path.
+      #
       # ===== Returns
-      # path<Pathname>::
-      #   path for which the event occured, or nil if no event occured
+      # path and type of event if event occured, nil otherwise
       #
       def detect_event
         path = @monitored_paths.max {|a,b| a.mtime <=> b.mtime }
